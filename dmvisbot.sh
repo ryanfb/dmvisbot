4078770fe5042b5623f69501e059d4345fe844be
Ryan Baumann <ryan.baumann@gmail.com>
2018-01-07 12:16:11 -0500

    Fix dmvisbot.sh

--- dmvisbot.sh
+++ dmvisbot.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export LC_ALL='en_US.UTF-8'
 export PYTHONIOENCODING='UTF-8'
-cd "$( dirname "${BASH_SOURCE[0]}" )" && python dmvisbot2.py >> /tmp/dmvisbot.log 2>&1
+export PYTHONPATH="$( dirname "${BASH_SOURCE[0]}" )/omgifol"
+cd "$( dirname "${BASH_SOURCE[0]}" )" && python2 dmvisbot2.py >> /tmp/dmvisbot.log 2>&1
